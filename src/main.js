3f28449b181075229cce1a729c13998bfb732b2a
Eden-95 <954537718@qq.com>
2020-09-15 11:13:50 +0800

    基本配置完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入通用样式
+import './styles/base.less'
+import './styles/iconfont.css'
+
+// 全局引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
